4c6ff45cddb3471b451378bc709124caa0fe0124
Jakubeus <126015243+Jakubeus@users.noreply.github.com>
2025-04-26 15:39:35 +0200

    version 0.3

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hello")
+
+
+const words = ["Usilovný", "Lojalny", "Ucenlivy", "Trpezlivy"];
+let index = 0;
+
+let wordContainer = document.querySelector(".word");
+
+function changeWord() {
+  index = (index + 1) % words.length;
+
+  const newWord = document.createElement("span");
+  newWord.className = "word";
+  newWord.textContent = words[index];
+
+  const oldWord = wordContainer;
+  oldWord.parentNode.replaceChild(newWord, oldWord);
+
+  wordContainer = newWord;
+}
+
+setInterval(changeWord, 2500);
