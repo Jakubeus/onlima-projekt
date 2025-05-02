5c9bed14e4046c529704b40a1ca22bc4c5c4b732
Jakubeus <126015243+Jakubeus@users.noreply.github.com>
2025-05-03 00:17:15 +0200

    final

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Hello")
 
 
-const words = ["Usilovný", "Lojalny", "Ucenlivy", "Trpezlivy"];
+const words = ["Usilovný", "Lojálný", "Učenlivý", "Trpezlivý"];
 let index = 0;
 
 let wordContainer = document.querySelector(".word");
@@ -20,3 +20,23 @@ function changeWord() {
 }
 
 setInterval(changeWord, 2500);
+
+const burger = document.getElementById('burger');
+const nav = document.getElementById('nav');
+const closeBtn = document.getElementById('close');
+const navLinks = document.querySelectorAll('#nav a');
+
+burger.addEventListener('click', () => {
+  nav.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+  nav.classList.remove('open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('open');
+  });
+});
+
